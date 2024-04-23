09b86ded673b8f72cc3a3bc7c12b9042b78d78cd
Mohitsen11 <mohitsen0102@gmail.com>
2024-04-23 21:53:46 +0530

    Added dashboard route

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,12 +1,38 @@
 "use client";
 
+import { api } from "@/convex/_generated/api";
+import { useConvex, useMutation, useQuery } from "convex/react";
 import { signOut, useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
-import React from "react";
+import React, { useEffect } from "react";
 
 function Dashboard() {
   const session = useSession();
   const router = useRouter();
+  const convex = useConvex();
+
+  const user = session?.data?.user;
+
+  // const getUser = useQuery(api.user.getUser, {email: user?.email});
+  const createUser = useMutation(api.user.createUser);
+  useEffect(() => {
+      if(user) {
+        checkUser();
+      }
+  }, [user]);
+
+  const checkUser = async () => {
+    const result = await convex.query(api.user.getUser, {email: user?.email});
+    if(!result?.length){
+      createUser({
+        name: user.name,
+        email: user.email,
+        image: user.image
+      }).then(res => {
+        console.log(res);
+      })
+    }
+  }
 
   if (session.status === "unauthenticated") {
     router.push("/");
